9b107ef8eac8093bcded63953f7d42e242a18cdc
Ronen <orlandronen@gmail.com>
2019-01-29 19:08:06 -0800

    Added some notes to traversal_routines.c
    Notes as reminders on inverting and non-inverting gates, defs of arrival time, required time, slack, and critical path finding method.

--- STA/traversal_routines.C
+++ STA/traversal_routines.C
@@ -19,4 +19,22 @@ void traversal_critical_path(EdgeS **pS, int &num_paths, Vertex *root,
 {
 }
 
+/* Notes
+
+   When dealing w/ non-inverting gates, add rising to rising and falling to falling
+   When dealing w/ inverting gates, add rising to next falling and falling to next rising
+
+   Arrival time (AT) = time elapsed for signal to arrive at certain point
+   Required time (RT) = latest time at which a signal can arrive to guarantee timing of output
+   Slack = RT - AT
+
+   Critical path is a path b/w input and output w/ maximum delay. Can have different paths for
+     rising/falling
+
+   Critical path method:
+     AT: traverse from input to output
+     RT: traverse from output to input
+     Slack: RT - AT
+     Critical path: search from output to input (highest R/F)
+*/
 // End
