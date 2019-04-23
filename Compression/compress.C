e47d42776b7722d29df3ff1bd373abe56318a39c
Ronen <orlandronen@gmail.com>
2019-04-22 18:17:50 -0700

    Frequency finding works
    Now uses hash map. Finds minterms, though not necessarily the one with most matches like the results do.

--- Compression/compress.C
+++ Compression/compress.C
@@ -7,6 +7,7 @@
 #define O '1'
 #define X '2'
 #define TRACE_COMPRESS 1
+#define TRACE_MATCHING 1
 
 #include "testVector.h"
 #include <math.h>
@@ -14,74 +15,13 @@
 
 
 // Recursively finds all matches to integer values for a given string
-vector<int> all_matches(string cube)
-{
-    vector<int> ret;
+void all_matches(string cube, std::map<string, int> &m);
 
-    // No X's in this string
-    if (cube.find(X) == std::string::npos)
-        ret.push_back(stoi(cube, nullptr, 2));
-    else    // There is an X. Replace it with 0 and 1 and call 2 more times.
-    {
-        string with_zero;
-        string with_one;
+// Finds keys of highest 2 values in a array
+vector<string> find_maxes(std::map<string, int> &m);
 
-        size_t index = cube.find(X);    // Find first X
-        if (index == 0)                 // First character
-        {
-            with_zero = cube.substr(0,1) + Z + cube.substr(2, string::npos);
-            with_one  = cube.substr(0,1) + O + cube.substr(2, string::npos);
-        }
-        else if (index == cube.length() - 1)    // Last character
-        {
-            with_zero = cube.substr(0,index) + Z;
-            with_one  = cube.substr(0,index) + O;
-        }
-        else    // Any other character
-        {
-            with_zero = cube.substr(0,index) + Z + cube.substr(index + 1, string::npos);
-            with_one  = cube.substr(0,index) + O + cube.substr(index + 1, string::npos);
-        }
-        
-        vector<int> first = all_matches(with_zero);
-        vector<int> second = all_matches(with_one);
-
-        ret.insert(ret.end(), first.begin(), first.end());
-        ret.insert(ret.end(), second.begin(), second.end());
-    }
-    
-    return ret;
-}
-
-// Finds indices of highest 2 values in an array
-vector<int> find_maxes(int arr[], int size)
-{
-    int max1 = -1;
-    int max2 = -1;
-    vector<int> indices (2,-1);
-
-    // Find first max
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] > max1)
-        {
-            max1 = arr[i];
-            indices.front() = i;
-        }
-    }
-
-    // Find second m ax
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] > max1 && arr[i] != max1)
-        {
-            max2 = arr[i];
-            indices.back() = i;
-        }
-    }
-
-    return indices;
-}
+// Checks if a term matches a string
+bool check_match(string cube, string term);
 
 /* You need to provide the solution to the testVector::compress routine. All
  * updates to the test vector set are to be done on the
@@ -107,21 +47,19 @@ void testVector::compress(int cubeWidth) {
     // For each vector in the set
     for (iter = testVectorSet.begin(); iter < testVectorSet.end(); iter++)
     {
-        // buff = string(*iter);
-        // For each term in the vector
-        for (int i = 0; i < vector_len; i += cubeWidth)
+        for (int i = 0; i < vector_len; i += cubeWidth) // For each term in the vector
         {
-            // vector<int> indices = all_matches(buff.substr(i,cubeWidth));
-            // vector<int> indices = all_matches(returnCube(*iter,i,cubeWidth));
-            // Increment frequencies of all matches values
-            // for (vector<int>::iterator j = indices.begin(); j < indices.end(); j++)
-                // freqs[*j]++;
-            freqs[ returnCube(*iter, i, cubeWidth) ]++;
+            #if TRACE_COMPRESS
+            cout << "TERM: " << returnCube(*iter, i, cubeWidth) << "\n";
+            #endif
+            all_matches(returnCube(*iter, i, cubeWidth), freqs);
         }
     }
 
-    // Get indices of minterms
-    // vector<int> minterm_indices = find_maxes(freqs, int(pow(2,cubeWidth)));
+    // Get minterms
+    vector<string> minterms = find_maxes(freqs);
+    m1 = minterms.front();
+    m2 = minterms.back();
 
 
     // Try to match as many terms w/ X's to the minterms
@@ -131,4 +69,72 @@ void testVector::compress(int cubeWidth) {
     // - If no match against either minterm, keep as is
     // -- append current term to buffer
     // - 
+}
+
+void all_matches(string cube, std::map<string, int> &m)
+{
+    // No X's in this string
+    if (cube.find(X) == std::string::npos)
+    {
+        m[cube]++;
+        #if TRACE_MATCHING
+        cout << "FREQ OF " << cube << " IS NOW " << m[cube] << "\n";
+        #endif
+    }
+    else    // There is an X. Replace it with 0 and 1 and call 2 more times.
+    {
+        string with_zero;
+        string with_one;
+        size_t index = cube.find(X);    // Find first X
+
+        if (index == 0)                 // First character
+        {
+            with_zero = Z + cube.substr(1, string::npos);
+            with_one  = O + cube.substr(1, string::npos);
+        }
+        else if (index == cube.length() - 1)    // Last character
+        {
+            with_zero = cube.substr(0,index) + Z;
+            with_one  = cube.substr(0,index) + O;
+        }
+        else    // Any other character
+        {
+            with_zero = cube.substr(0,index) + Z + cube.substr(index + 1, string::npos);
+            with_one  = cube.substr(0,index) + O + cube.substr(index + 1, string::npos);
+        }
+        
+        all_matches(with_zero, m);
+        all_matches(with_one, m);
+    }
+
+}
+
+vector<string> find_maxes(std::map<string, int> &m)
+{
+    int max1 = -1;
+    int max2 = -1;
+    vector<string> keys (2, "");
+
+    // Find first max
+    for (auto i = m.begin(); i != m.end(); i++)
+        if (i->second > max1)
+        {
+            max1 = i->second;
+            keys.front() = i->first;
+        }
+
+    // Find second max
+    for (auto i = m.begin(); i != m.end(); i++)
+        if (i->second > max2 && i->first != keys.front())
+        {
+            max2 = i->second;
+            keys.back() = i->first;
+        }
+
+    return keys;
+}
+
+bool check_match(string cube, string term)
+{
+    
 }
